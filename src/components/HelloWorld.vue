272b292601a7765564173395961d5feb713b5fb6
Bence Kerner <kernerdev@gmail.com>
2020-10-23 18:46:25 +0200

    init files

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <button v-on:click="getData">getData</button>
+  <p id="message">{{info}}</p>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      info: null
+    }
+  },
+  methods: {
+    getData(){
+      axios
+        .get('http://127.0.0.1:1234/hello')
+        .then(response => (this.info = response.data.message))
+        .catch(error => this.info = error)
+    }
+  }
+}
+</script>
